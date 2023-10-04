71de00f92cb147fae5f7749ea114ba3b521f980a
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-10-04 12:47:46 +0200

    Changed  String toFailTest = "Change for branch1";

--- src/test/java/com/example/devops_group_examination6/controllers/MenuControllerTest.java
+++ src/test/java/com/example/devops_group_examination6/controllers/MenuControllerTest.java
@@ -35,7 +35,7 @@ class MenuControllerTest {
     public void getTodaysMenuTest() throws Exception {
         String todaysDate = LocalDate.now().getDayOfWeek().toString();
         String expectedMenu = "Todays Menu";
-        String toFailTest = "Fail menu test12";
+        String toFailTest = "Change for branch1";
         when(menuManipulator.checkTodaysMenu(todaysDate)).thenReturn(expectedMenu);
 
         mockMvc.perform(get("/menu/today"))
